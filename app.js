508079d8479485e04bc6e95aeffff105679f942e
AlexHeeee <1085250220alex@gmail.com>
2021-09-18 17:21:38 +0800

    add start script

--- app.js
+++ app.js
@@ -96,7 +96,9 @@ app.use("/Universities", universityRoutes);
 app.use('/Universities/:id/reviews', reviewRoutes);
 
 
-app.listen(3000, ()=> {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Serving on port ${port}`)
 })
 
